6604c933c0fce1518785f059d21cf976bafe9b64
jeddyn <zar1117@o2.pl>
2018-05-27 18:00:19 +0200

    ad

--- src/main/java/PA0/Main.java
+++ src/main/java/PA0/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Created by Mateusz on 2017-05-20
+ * Created by Mateusz on 2018-05-20
  */
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,8 +19,24 @@ public class Main {
         {
             base = scanner.nextInt();
             index = scanner.nextInt();
-            ModularAlgorithm modularAlgorithm = new ModularAlgorithm();
-            System.out.println(String.valueOf(modularAlgorithm.pModular(base,index)));
+            System.out.println(String.valueOf(pModular(base,index)));
         }
+    }
+    private static long pModular(int base, int index){
+        int i;
+        int result = 1;
+        final int modulation = 10;
+        long x = base%modulation;
+
+        for (i=1; i<=index; i<<=1) {
+            x %= modulation;
+            if ((index&i) != 0) {
+                result *= x;
+                result %= modulation;
+            }
+            x *= x;
+        }
+
+        return result;
     }
 }
